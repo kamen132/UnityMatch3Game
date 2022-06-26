42934a6e502984f104a45490c98ed54326a36a53
kamen <mymusic990610@163.com>
2022-06-26 08:46:54 +0800

    三消game

--- Assets/Match3Game2/Scripts/UI/LevelType.cs
+++
@@ -1,6 +0,0 @@
-public enum LevelType
-{
-    TIMER,
-    OBSTACLE,
-    MOVES,
-}
---
+++ Assets/Scripts/Object/PLayerInfo.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PLayerInfo : MonoBehaviour
+{
+
+    public static PLayerInfo Info;      // infomations of player
+
+    public static Player MapPlayer;     // player object
+
+    public static byte MODE;            // mode : Arcade or Classic 
+
+    public static int BACKGROUND;       // background of mode
+
+    public int Score;
+
+    public const string KEY_CLASSIC_HISCORE = "classichightscore";
+
+    public TextMesh textlv;
+
+    void Awake()
+    {
+        PLayerInfo.MapPlayer = new Player();
+        Info = this;
+       // BACKGROUND = MapPlayer.Background;
+        MapPlayer.Level = 86;
+    }
+
+    void Start()
+    {
+        Score = 0;
+        EffectSpawner.effect.SetLevel(MapPlayer.Level);
+        Debug.LogError("Level=="+MapPlayer.Level);
+        EffectSpawner.effect.SetBest(MapPlayer.HightScore);
+        EffectSpawner.effect.SetScore(Score);
+        textlv.text = MapPlayer.Level.ToString();
+    }
+}
